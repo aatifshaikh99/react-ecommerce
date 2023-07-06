5e5d941ee8bafebfc55a8a43785f88fc497648a4
aatifshaikh99 <aatifshaikh821@gmail.com>
2023-07-06 13:39:09 +0530

    mock key

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,14 +5,15 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FB_API_KEY || 'mock_key',
-  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
-  projectId: "eshop-e0f70",
-  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FB_API_MSG_SENDER_ID,
-  appId: process.env.REACT_APP_FB_APP_ID,
-}; 
- 
+  apiKey: process.env.REACT_APP_FB_API_KEY || "mock_key",
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN || "authentication_key",
+  projectId: process.env.REACT_APP_FB_PROJECT_ID || "project_id",
+  storageBucket:
+    process.env.REACT_APP_FB_STORAGE_BUCKET || "storage_bucket_key",
+  messagingSenderId: process.env.REACT_APP_FB_API_MSG_SENDER_ID || "sender_id",
+  appId: process.env.REACT_APP_FB_APP_ID || "firebase_app_id",
+};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
