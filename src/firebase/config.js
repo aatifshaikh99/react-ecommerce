ca8e30bc198554b532cbe819ebc32692970858a8
aatifshaikh99 <aatifshaikh821@gmail.com>
2023-07-06 13:31:51 +0530

    key

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,7 +5,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FB_API_KEY,
+  apiKey: process.env.REACT_APP_FB_API_KEY || 'mock_key',
   authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
   projectId: "eshop-e0f70",
   storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
